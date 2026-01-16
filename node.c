9c9a26d2468d2d38f0b7e980ed11359e14e8debf
pythonicoder <159662601+pythonicoder@users.noreply.github.com>
2026-01-16 21:08:24 +0100

    Add files via upload

---
+++ node.c
@@ -0,0 +1,15 @@
+#include "push_swap.h"
+
+t_node *new_node(int value)
+{
+	t_node	*node;
+
+	node = malloc(sizeof(t_node));
+	if (!node)
+		return (NULL);
+	node->value = value;
+	node->index = -1;
+	node->next = node;
+	node->prev = node;
+	return (node);
+}
